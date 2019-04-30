e45039cf96061ebcb08d129cfb17b09e51cf69fe
gurux01 <gurux@gurux.fi>
2019-04-30 15:36:49 +0300

    Support for IGXMedia2 added.

--- GXTerminal csharp Sample/AssemblyInfo.cs
+++ GXTerminal csharp Sample/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 //
 // --------------------------------------------------------------------------
 //  Gurux Ltd
-// 
+//
 //
 //
 // Filename:        $HeadURL: svn://utopia/projects/Old/GuruxTerminal/GXTerminal%20csharp%20Sample/AssemblyInfo.cs $
@@ -19,16 +19,16 @@
 // This file is a part of Gurux Device Framework.
 //
 // Gurux Device Framework is Open Source software; you can redistribute it
-// and/or modify it under the terms of the GNU General Public License 
+// and/or modify it under the terms of the GNU General Public License
 // as published by the Free Software Foundation; version 2 of the License.
 // Gurux Device Framework is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of 
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 // See the GNU General Public License for more details.
-// 
+//
 // More information of Gurux Terminal (Modem) solution : http://www.gurux.org/GXTerminal
 //
-// This code is licensed under the GNU General Public License v2. 
+// This code is licensed under the GNU General Public License v2.
 // Full text may be retrieved at http://www.gnu.org/licenses/gpl-2.0.txt
 //---------------------------------------------------------------------------
 
@@ -56,7 +56,7 @@ using System.Runtime.InteropServices;
 [assembly:AssemblyDescription("")]
 [assembly:AssemblyCompany("Gurux")]
 [assembly:AssemblyProduct("")]
-[assembly: AssemblyCopyright("Copyright (c) 1998-2018 Gurux Ltd. All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright (c) 1998-2019 Gurux Ltd. All rights reserved.")]
 [assembly:AssemblyTrademark("")]
 [assembly:AssemblyCulture("")]
 
